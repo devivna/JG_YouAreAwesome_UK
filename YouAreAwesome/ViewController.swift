d47a6e53f8a2eda643a9559078440649aa86e9e8
Nadia <ivashchenko.nadia@gmail.com>
2023-01-23 20:45:47 +0200

    first init

--- YouAreAwesome/ViewController.swift
+++ YouAreAwesome/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
 
+        if messageLabel.text == "Hello" {
+            messageLabel.text = "Hi, sweety!"
+            imageView.image = UIImage(named: "Book2")
+        } else {
+            messageLabel.text = "Hello"
+            imageView.image = UIImage(named: "Book1")
+        }
+        
+    }
+    
 }
 
