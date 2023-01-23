508f05345adbeacc0a026af2ca8804ca2035f6d6
Nadia <ivashchenko.nadia@gmail.com>
2023-01-23 23:07:16 +0200

    Condition statement with label + image

--- YouAreAwesome/ViewController.swift
+++ YouAreAwesome/ViewController.swift
@@ -12,20 +12,44 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
+    var imageNumber = 1
+    var messageNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        var messages = [
+            "This is Book1",
+            "This is Book2",
+            "This is Book3",
+            "This is Book4",
+            "This is Book5",
+        ]
 
-        if messageLabel.text == "Hello" {
-            messageLabel.text = "Hi, sweety!"
-            imageView.image = UIImage(named: "Book2")
-        } else {
-            messageLabel.text = "Hello"
-            imageView.image = UIImage(named: "Book1")
+        //let imageName = "Book" + String(imageNumber)
+        imageView.image = UIImage(named: "Book\(imageNumber)")
+        messageLabel.text = messages[messageNumber]
+        
+        imageNumber += 1
+        messageNumber += 1
+        
+        if messageNumber == messages.count {
+            imageNumber = 1
+            messageNumber = 0
         }
+        
+        
+//        if messageLabel.text == "Hello" {
+//            messageLabel.text = "Hi, sweety!"
+//            imageView.image = UIImage(named: "Book2")
+//        } else {
+//            messageLabel.text = "Hello"
+//            imageView.image = UIImage(named: "Book1")
+//        }
         
     }
     
